3a9bc7a7f01a07cd08e1dfd7ddb6d8376c6820d9
timberdo <44552608+timberdo@users.noreply.github.com>
2019-03-22 18:55:53 +0300

    Add files via upload
    - marked-up code according to PEP8

--- labblank/PlateletCalculator.py
+++ labblank/PlateletCalculator.py
@@ -1,37 +1,37 @@
 import random
 
 # Задаем входящие параметры
-volume=(int(input("Введите объем дозы в мл:")))/1000
-int1=int(input("Первая клетка:"))
-int2=int(input("Вторая клетка:"))
-int3=int(input("Третья клетка:"))
+volume = (int(input("Введите объем дозы в мл:"))) / 1000
+int1 = int(input("Первая клетка:"))
+int2 = int(input("Вторая клетка:"))
+int3 = int(input("Третья клетка:"))
 
 # Получаем  примерное количество клеток
-bigint=((int1+int2+int2)/3)*25
+bigint = ((int1 + int2 + int2) / 3) * 25
 
 # Производим расчет количества клеток в каждой из 25 клеток
-analog=bigint
-counter=25
-list=[]
-while counter>0:
-    percent=random.randint(1,25)
-    pluscorrect=(bigint/25)+(((bigint/25)/100)*percent)
-    minuscorrect=(bigint/25)-(((bigint/25)/100)*percent)
-    pendulum=random.randint(0,1)
-    if pendulum==0:
-        analog=analog-pluscorrect
+analog = bigint
+counter = 25
+list = []
+while counter > 0:
+    percent = random.randint(1, 25)
+    pluscorrect = (bigint / 25) + (((bigint / 25) / 100) * percent)
+    minuscorrect = (bigint / 25) - (((bigint / 25) / 100) * percent)
+    pendulum = random.randint(0, 1)
+    if pendulum == 0:
+        analog = analog - pluscorrect
         list.append(int(pluscorrect))
     else:
-        analog=analog-minuscorrect
+        analog = analog - minuscorrect
         list.append(int(minuscorrect))
-    counter-=1
+    counter -= 1
 
-# Подсчитываем общее количество клеток    
-total=sum(list)
+# Подсчитываем общее количество клеток
+total = sum(list)
 
 # Получаем результат
-result=(total*2)*volume
+result = (total * 2) * volume
 
 print(list)
 print('Итого:', total)
-print('Результат: %3.0f' %(result))
+print('Результат: %3.0f' % (result))
