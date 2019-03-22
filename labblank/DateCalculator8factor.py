6f99acd8f0cfd6f63c268a1fe4b8635270cbde0d
timberdo <44552608+timberdo@users.noreply.github.com>
2019-03-22 18:54:55 +0300

    Add files via upload
    - added comments
    - marked-up code according to PEP8

--- labblank/DateCalculator8factor.py
+++ labblank/DateCalculator8factor.py
@@ -1,17 +1,23 @@
-import datetime 
+#Импорт библиотек
+import datetime
 from dateutil.relativedelta import relativedelta
 
-dateSelect=input('Дата отбора в формате ДД.ММ.ГГГГ:')
-dateRelease=input('Дата изготовления в формате ДД.ММ.ГГГГ:')
+#Получаем от пользователя строки дат
+dateSelect = input('Дата отбора в формате ДД.ММ.ГГГГ:')
+dateRelease = input('Дата изготовления в формате ДД.ММ.ГГГГ:')
 
+#Рассортировываем полученные даты по переменным
 dayS, monthS, yearS = map(int, dateSelect.split('.'))
 dayR, monthR, yearR = map(int, dateRelease.split('.'))
 
-dateS=datetime.date(day=dayS, month=monthS, year=yearS)
-dateR=datetime.date(day=dayR, month=monthR, year=yearR)
+#Преобразуем полученные переменные в тип datetime
+dateS = datetime.date(day=dayS, month=monthS, year=yearS)
+dateR = datetime.date(day=dayR, month=monthR, year=yearR)
 
-diffDelta=relativedelta(dateS, dateR)
+#Производим вычисление
+diffDelta = relativedelta(dateS, dateR)
 
+#Результат
 print('Лет:', diffDelta.years)
 print('Месяцев:', diffDelta.months)
-print('Дней:', diffDelta.days+1)
+print('Дней:', diffDelta.days + 1)
