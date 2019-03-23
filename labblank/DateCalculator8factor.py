8d2e284d60888af3ce47490455c0bc4715bfb0ec
timberdo <44552608+timberdo@users.noreply.github.com>
2019-03-23 12:23:21 +0300

    Add files via upload
    - added the possibility of cyclic input of the date of manufacture
    - added the function to exit script
    - changed the format of the input dates to a more convenient

--- labblank/DateCalculator8factor.py
+++ labblank/DateCalculator8factor.py
@@ -1,23 +1,33 @@
 #Импорт библиотек
 import datetime
 from dateutil.relativedelta import relativedelta
+from sys import stdin, exit as sys_exit
+
+a = str('exit')
 
 #Получаем от пользователя строки дат
-dateSelect = input('Дата отбора в формате ДД.ММ.ГГГГ:')
-dateRelease = input('Дата изготовления в формате ДД.ММ.ГГГГ:')
+dateSelect = input('Дата отбора в формате ДД ММ ГГГГ(разделены пробелами):')
 
+while True:
+    dateRelease = input('Дата изготовления в формате ДД ММ ГГГГ(разделены пробелами):')
+    if dateRelease == a:
+        sys_exit()
+    else:
 #Рассортировываем полученные даты по переменным
-dayS, monthS, yearS = map(int, dateSelect.split('.'))
-dayR, monthR, yearR = map(int, dateRelease.split('.'))
+        dayS, monthS, yearS = map(int, dateSelect.split(' '))
+        dayR, monthR, yearR = map(int, dateRelease.split(' '))
 
 #Преобразуем полученные переменные в тип datetime
-dateS = datetime.date(day=dayS, month=monthS, year=yearS)
-dateR = datetime.date(day=dayR, month=monthR, year=yearR)
+        dateS = datetime.date(day=dayS, month=monthS, year=yearS)
+        dateR = datetime.date(day=dayR, month=monthR, year=yearR)
 
 #Производим вычисление
-diffDelta = relativedelta(dateS, dateR)
+        diffDelta = relativedelta(dateS, dateR)
 
 #Результат
-print('Лет:', diffDelta.years)
-print('Месяцев:', diffDelta.months)
-print('Дней:', diffDelta.days + 1)
+        print('Год(а):', diffDelta.years)
+        print('Месяцев:', diffDelta.months)
+        print('Дней:', diffDelta.days + 1)
+        print('=======================================')
+        print('Для изменения даты отбора перезапустите программу')
+        print('Для выхода наберите exit')
