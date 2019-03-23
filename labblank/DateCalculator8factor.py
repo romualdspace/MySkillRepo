133945c9a54e6d16c565348fc3232ccf12b73444
timberdo <44552608+timberdo@users.noreply.github.com>
2019-03-23 14:10:33 +0300

    Add files via upload
    - added some visual decorations

--- labblank/DateCalculator8factor.py
+++ labblank/DateCalculator8factor.py
@@ -1,34 +1,44 @@
-#Импорт библиотек
+# Импорт библиотек
 import datetime
 from dateutil.relativedelta import relativedelta
 from sys import stdin, exit as sys_exit
+from colorama import init
+from termcolor import colored
+
 
 a = str('exit')
 
-#Получаем от пользователя строки дат
-dateSelect = input('Дата отбора в формате ДД ММ ГГГГ(разделены пробелами):')
+# Получаем от пользователя строки дат
+dateSelect = input(
+    'Дата отбора в формате ДД ММ ГГГГ(разделены пробелами):' '\n')
 
-#Циклический ввод даты изготовления
+# Циклический ввод даты изготовления
 while True:
-    dateRelease = input('Дата изготовления в формате ДД ММ ГГГГ(разделены пробелами):')
-    if dateRelease == a:
-        sys_exit()
-    else:
-#Рассортировываем полученные даты по переменным
-        dayS, monthS, yearS = map(int, dateSelect.split(' '))
-        dayR, monthR, yearR = map(int, dateRelease.split(' '))
+    try:
+        dateRelease = input(
+            'Дата изготовления в формате ДД ММ ГГГГ(разделены пробелами):' '\n')
+        if dateRelease == a:
+            sys_exit()
+        else:
+            # Рассортировываем полученные даты по переменным
+            dayS, monthS, yearS = map(int, dateSelect.split(' '))
+            dayR, monthR, yearR = map(int, dateRelease.split(' '))
 
-#Преобразуем полученные переменные в тип datetime
-        dateS = datetime.date(day=dayS, month=monthS, year=yearS)
-        dateR = datetime.date(day=dayR, month=monthR, year=yearR)
+# Преобразуем полученные переменные в тип datetime
+            dateS = datetime.date(day=dayS, month=monthS, year=yearS)
+            dateR = datetime.date(day=dayR, month=monthR, year=yearR)
 
-#Производим вычисление
-        diffDelta = relativedelta(dateS, dateR)
+# Производим вычисление
+            diffDelta = relativedelta(dateS, dateR)
 
-#Результат
-        print('Год(а):', diffDelta.years)
-        print('Месяцев:', diffDelta.months)
-        print('Дней:', diffDelta.days + 1)
-        print('=======================================')
-        print('Для изменения даты отбора перезапустите программу')
-        print('Для выхода наберите exit')
+# Результат
+            print(colored('Год(а): ', 'blue'), diffDelta.years)
+            print(colored('Месяцев:', 'blue'), diffDelta.months)
+            print(colored('Дней:   ', 'blue'), diffDelta.days + 1)
+            print('=======================================')
+            print('Для изменения даты отбора перезапустите программу')
+            print('Для выхода наберите exit')
+            print('=======================================')
+    except ValueError:
+        print(colored(
+            'Дата введена неверно. Перепроверьте правильность введеной даты.', 'white', 'on_red'))
