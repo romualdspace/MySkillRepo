880cb0a6c8baf0efda7f215df718574b4dbed170
timberdo <44552608+timberdo@users.noreply.github.com>
2019-03-24 17:26:25 +0300

    Add files via upload
    - added error handling

--- labblank/DateCalculator8factor.py
+++ labblank/DateCalculator8factor.py
@@ -5,13 +5,14 @@ from sys import stdin, exit as sys_exit
 from colorama import init
 from termcolor import colored
 
-
+# Задаем эталонные переменные
 a = str('выход')
 b = str('exit')
 monthCounter = [ai for ai in range(1, 13)]
 dayCounter = [bi for bi in range(1, 32)]
 yearCounter = [ci for ci in range(1900, 2200)]
 
+# Цикл проверки введенный даты ОТБОРА пробы
 while True:
     try:
         dateSelect = input(
@@ -19,42 +20,53 @@ while True:
         if a == dateSelect or b == dateSelect:
             sys_exit()
         else:
-            dayS, monthS, yearS = map(int, dateSelect.split(' '))
-            dateS = datetime.date(day=dayS, month=monthS, year=yearS)
+            if len(dateSelect) == 10:
+                dayS, monthS, yearS = map(int, dateSelect.split(' '))
+                if monthS in monthCounter and dayS in dayCounter and yearS in yearCounter:
+                    dateS = datetime.date(day=dayS, month=monthS, year=yearS)
+                else:
+                    raise ValueError
+            else:
+                raise ValueError
+
     except (ValueError, NameError):
         print(colored(
-            'Дата введена неверно. Проверьте правильность введеной даты.', 'white', 'on_red'))
-    if len(dateSelect) == 10 and monthS in monthCounter and dayS in dayCounter and yearS in yearCounter:
-        while True:
-            try:
-                dateRelease = input(
-                    'Дата изготовления в формате ДД ММ ГГГГ(разделены пробелами):' '\n    ')
-                if a == dateRelease or b == dateRelease:
-                    sys_exit()
-                else:
+            'Дата отбора введена неверно. Проверьте правильность введеной даты.', 'white', 'on_red'))
+        continue
+
+# Цикл проверки введенный даты ИССЛЕДОВАНИЯ пробы
+    while True:
+        try:
+            dateRelease = input(
+                'Дата изготовления в формате ДД ММ ГГГГ(разделены пробелами):' '\n    ')
+            if a == dateRelease or b == dateRelease:
+                sys_exit()
+            else:
+                if len(dateRelease) == 10:
                     dayR, monthR, yearR = map(int, dateRelease.split(' '))
-                    dateR = datetime.date(day=dayR, month=monthR, year=yearR)
+                    if monthR in monthCounter and dayR in dayCounter and yearR in yearCounter:
+                        dateR = datetime.date(
+                            day=dayR, month=monthR, year=yearR)
 
-                    if len(dateRelease) == 10 and monthS in monthCounter and dayS in dayCounter and yearS in yearCounter:
-
+#Вычисление разницы дат
                         diffDelta = relativedelta(dateS, dateR)
 
                         print('-----------------')
-                        print(colored('    Год(а):    ', 'blue'), diffDelta.years)
+                        print(colored('    Год(а):    ', 'blue'),
+                              diffDelta.years)
                         print(colored('    Месяца(ев):', 'blue'),
                               diffDelta.months)
                         print(colored('    День(ей):  ', 'blue'),
                               diffDelta.days + 1)
                         print('-----------------')
-                        print('Для изменения даты отбора перезапустите программу')
+                        print(
+                            'Для изменения даты отбора перезапустите программу')
                         print('Для выхода наберите exit или выход')
                         print('=======================================')
                     else:
-                        print(colored(
-                            'Дата введена неверно. Проверьте правильность введеной даты.', 'white', 'on_red'))
-            except (ValueError, NameError):
-                print(colored(
-                    'Дата введена неверно. Проверьте правильность введеной даты.', 'white', 'on_red'))
-    else:
-        print(colored(
-            'Дата введена неверно. Проверьте правильность введеной даты.', 'white', 'on_red'))
+                        raise ValueError
+                else:
+                    raise ValueError
+        except (ValueError, NameError):
+            print(colored(
+                'Дата изготовления введена неверно. Проверьте правильность введеной даты.', 'white', 'on_red'))
