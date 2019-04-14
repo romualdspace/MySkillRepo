6231602a88555e3bf9be167c2bd1c12ed89fed1f
timberdo <44552608+timberdo@users.noreply.github.com>
2019-04-14 18:44:31 +0300

    - changed input method

--- labblank/DC8f_not_colorized.py
+++ labblank/DC8f_not_colorized.py
@@ -2,8 +2,6 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 from sys import stdin, exit as sys_exit
-from colorama import init
-from termcolor import colored
 
 # Задаем эталонные переменные
 a = str('выход')
@@ -16,12 +14,12 @@ yearCounter = [ci for ci in range(1900, 2200)]
 while True:
     try:
         dateSelect = input(
-            'Дата отбора в формате ДД ММ ГГГГ(разделены точками):' '\n    ')
+            'Дата отбора в формате ДД ММ ГГГГ(разделены пробелами):' '\n    ')
         if a == dateSelect or b == dateSelect:
             sys_exit()
         else:
             if len(dateSelect) == 10:
-                dayS, monthS, yearS = map(int, dateSelect.split('.'))
+                dayS, monthS, yearS = map(int, dateSelect.split(' '))
                 if monthS in monthCounter and dayS in dayCounter and yearS in yearCounter:
                     dateS = datetime.date(day=dayS, month=monthS, year=yearS)
                 else:
@@ -38,12 +36,12 @@ while True:
     while True:
         try:
             dateRelease = input(
-                'Дата изготовления в формате ДД ММ ГГГГ(разделены точками):' '\n    ')
+                'Дата изготовления в формате ДД ММ ГГГГ(разделены пробелами):' '\n    ')
             if a == dateRelease or b == dateRelease:
                 sys_exit()
             else:
                 if len(dateRelease) == 10:
-                    dayR, monthR, yearR = map(int, dateRelease.split('.'))
+                    dayR, monthR, yearR = map(int, dateRelease.split(' '))
                     if monthR in monthCounter and dayR in dayCounter and yearR in yearCounter:
                         dateR = datetime.date(
                             day=dayR, month=monthR, year=yearR)
